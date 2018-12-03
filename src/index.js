7168f98a1cc515a13a81a4a4e88f2a926ddb0577
hornuglan <hornuglan@gmail.com>
2018-12-03 16:09:06 +0300

    changing players was added

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
