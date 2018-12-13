e6a242bcc54bb5ebdb2c0de9f734b0cdd9c715f5
hornuglan <hornuglan@gmail.com>
2018-12-13 22:11:49 +0300

    render function was updated to show current game status

--- src/index.js
+++ src/index.js
@@ -88,13 +88,17 @@ const Square = (props) => {
       } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+            squares = {current.squares}
+            onClick = {(i) => this.handleClick(i)}
+             />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
