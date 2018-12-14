48ac056f0922f3c8b5a8915314b90fdc28821f4b
hornuglan <hornuglan@gmail.com>
2018-12-14 17:18:00 +0300

    render function in Board class was modified

--- src/index.js
+++ src/index.js
@@ -34,18 +34,10 @@ const Square = (props) => {
     }
   
     render() {
-    // const winner = calculateWinner(this.state.squares);
-    // let status;
-
-    // if (winner) {
-    //     status = 'Winner: ' + winner;
-    // } else {
-    //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    // }
-  
+    
       return (
         <div>
-          <div className="status">{status}</div>
+          {/* <div className="status">{status}</div> */}
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
